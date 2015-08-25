ba77d283de974a87eb8b6caf768ebf64549bbe77
Chris Bennetts-Cash <chris@bennettscash.id.au>
2015-08-26 08:17:42 +1000

    Hit flickr and get recent photos

--- app/src/main/java/au/id/bennettscash/photogallery/FlickrFetchr.java
+++ app/src/main/java/au/id/bennettscash/photogallery/FlickrFetchr.java
@@ -1,5 +1,8 @@
 package au.id.bennettscash.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +13,16 @@ import java.net.URL;
  * Created by chris on 24/08/15.
  */
 public class FlickrFetchr {
+    public static final String TAG = "FlickrFetchr";
+
+    private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+    private static final String API_KEY = "f575ce257eb85a435e8d84fab16678b7";
+    private static final String API_SECRET = "2b916b314c9dd1ef";
+    private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    private static final String PARAM_EXTRAS = "extras";
+
+    private static final String EXTRA_SMALL_URL = "url_s";
+
     byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
@@ -36,5 +49,19 @@ public class FlickrFetchr {
 
     public String getUrl(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse(ENDPOINT).buildUpon()
+                    .appendQueryParameter("method", METHOD_GET_RECENT)
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                    .build().toString();
+            String xmlString = getUrl(url);
+            Log.i(TAG, "Received XML: " + xmlString);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to fetch items", e);
+        }
     }
 }
