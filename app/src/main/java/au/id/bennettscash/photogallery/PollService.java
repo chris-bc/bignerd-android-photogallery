84fe1792db76670201bcb4eabf92c2a8aaae5fe7
Chris Bennetts-Cash <chris@bennettscash.id.au>
2015-09-02 22:23:36 +1000

    simple notification

--- app/src/main/java/au/id/bennettscash/photogallery/PollService.java
+++ app/src/main/java/au/id/bennettscash/photogallery/PollService.java
@@ -2,10 +2,13 @@ package au.id.bennettscash.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -48,6 +51,25 @@ public class PollService extends IntentService {
 
         if (!resultId.equals(lastResultId)) {
             Log.i(TAG, "Got a new result: " + resultId);
+
+            Resources r = getResources();
+            PendingIntent pi = PendingIntent.getActivity(this,
+                    0, new Intent(this, PhotoGalleryActivity.class), 0);
+
+            Notification notification = new Notification.Builder(this)
+                    .setTicker(r.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(r.getString(R.string.new_pictures_title))
+                    .setContentText(r.getString(R.string.new_pictures_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+
+            NotificationManager notificationManager = (NotificationManager)
+                    getSystemService(NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0, notification);
+
             prefs.edit()
                     .putString(FlickrFetchr.PREF_LAST_RESULT_ID, resultId)
                     .commit();
